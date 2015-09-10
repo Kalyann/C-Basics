613f56ecabd61d8fb6c4faf59fcaa87d70cabdb0
Kalyan_Lahkar <klahkar25@gmail.com>
2015-09-10 12:24:08 +0530

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,6 @@
+#include<stdio.h>//header file stdio.h which contains the printf function
+int main()
+{
+ printf("Hello World");// prints to output device
+ return 0;//denotes successfull termination of the program
+ }
